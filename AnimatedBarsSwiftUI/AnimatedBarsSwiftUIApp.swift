c542639e61a521188c6e10b30a580515880a523d
Clinton Davelaar <Davelaarprogramming@gmail.com>
2021-02-05 11:57:20 -0600

    Initial Commit

---
+++ AnimatedBarsSwiftUI/AnimatedBarsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedBarsSwiftUIApp.swift
+//  AnimatedBarsSwiftUI
+//
+//  Created by Davelaar,Clinton B on 2/5/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedBarsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
